0b0eb44dd64c7d53c04f2aac94cfe9ecaa41f824
Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
2023-08-31 14:00:26 -0700

    Added Avatars plugin
    Signed-off-by: Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>

---
+++ Avatars/Avatar.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Plugins\Avatars;
+
+use Phoundation\Accounts\Interfaces\UserInterface;
+use Phoundation\Content\Images\Image;
+use Phoundation\Content\Images\Interfaces\ImageInterface;
+use Phoundation\Core\Config;
+use Phoundation\Exception\OutOfBoundsException;
+use Plugins\Avatars\Robohash\Robohash;
+
+
+/**
+ * Avatar class
+ *
+ * This class manages and generate user and project avatars
+ *
+ * @author Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @copyright Copyright (c) 2023 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @package Plugins/Avatars
+ */
+class Avatar
+{
+    /**
+     * Generate a new user avatar
+     *
+     * @param UserInterface $user
+     * @param string|null $generator
+     * @return ImageInterface
+     */
+    public static function generate(UserInterface $user, ?string $generator = null): ImageInterface
+    {
+        return match (Config::get('users.avatars.generator', 'robohash', $generator)) {
+            'gravatar' => Gravatar::generate($user),
+            'robohash' => Robohash::generate($user),
+            default    => throw new OutOfBoundsException(tr('Unknown avatar generator ":generator" specified', [
+                ':generator' => $generator
+            ])),
+        };
+    }
+}
---
+++ Avatars/Robohash/Robohash.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Plugins\Avatars\Robohash;
+
+use Phoundation\Accounts\Users\Interfaces\UserInterface;
+use Phoundation\Content\Images\Image;
+use Phoundation\Content\Images\Interfaces\ImageInterface;
+use Phoundation\Filesystem\Path;
+use Phoundation\Filesystem\Restrictions;
+use Phoundation\Web\Http\File;
+
+
+/**
+ * Robohash class
+ *
+ * This class can connect to the robohash website and download and install robohash avatar images
+ *
+ * @author Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @copyright Copyright (c) 2023 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @package Plugins/Avatars
+ */
+class Robohash
+{
+    /**
+     * Generate a new avatar and assign it to
+     *
+     * @param UserInterface $user
+     * @return ImageInterface
+     */
+    public static function generate(UserInterface $user): ImageInterface
+    {
+        $restrictions = Restrictions::new(PATH_DATA, true);
+        $path         = PATH_DATA . 'content/cdn/en/img/profiles/' . $user->getLogId();
+
+        $picture      = File::new($restrictions)->download('https://robohash.org/' . $user->getDisplayName(), function ($file) use ($restrictions, $user, $path) {
+            $path    = Path::new($path, $restrictions)->ensure();
+            $picture = Image::new($path . 'profile.png');
+
+            rename($file, $picture);
+            $user->setPicture($picture);
+
+            return $picture;
+        });
+
+        return new Image($picture);
+    }
+}
