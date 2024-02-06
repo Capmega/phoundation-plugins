5eed1d63cd7c3371eb6249c5c19735d03ed1fcdc
Sven Olaf Oostenbrink <sven@medinet.ca>
2024-02-06 14:44:46 -0800

    Fixed documentation
    Signed-off-by: Sven Olaf Oostenbrink <sven@medinet.ca>

--- Plugins/JavaScriptCopy/JavaScriptCopy.php
+++ Plugins/JavaScriptCopy/JavaScriptCopy.php
@@ -20,7 +20,7 @@ use Phoundation\Web\Http\Html\Traits\Rendered;
  * @author Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
  * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
  * @copyright Copyright (c) 2022 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
- * @package Company\Data
+ * @package Phoundation\Data
  */
 class JavaScriptCopy extends Script
 {
